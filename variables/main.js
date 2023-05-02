c4fc46d413b6c778fb5249c860aa1f6cc2ba5130
Azi-mx <bagsariyaa@gmail.com>
2023-05-02 15:37:50 +0530

    changes

---
+++ variables/main.js
@@ -0,0 +1,42 @@
+// // document.getElementById('demo').innerHTML = 'starting this javascript with this paragraph';
+// document.getElementById("demo").innerHTML = "Hello JavaScript";
+// alert('Hi')
+// console.log('Hi');
+// document.write('hi')
+// window.alert('hello')
+
+
+/*Q1
+let age = prompt("what is your age?");
+if(age > 10 && age<20){
+    console.log("Your age lies between 10 and 20");
+}
+else{
+    console.log('Your age is less or more');
+}
+*/
+
+
+//Q2
+/*let age = prompt("what is your age? ");
+switch (age){
+    case '12':
+        console.log('Your age is 12');
+    case '13':
+            console.log('Your age is 13');
+            case '14':
+                console.log('Your age is 14');
+                case '15':
+            console.log('Your age is 15');
+     default:
+        console.log('rrrrrrr');       
+}*/
+
+//Q3
+let num = prompt("Enter the number");
+if(num%2==0 && num%3==0){
+    console.log('yes');
+}
+else{
+    console.log('no');
+}
