9eb595d65b6f94111ea6cd75987b0586472956d5
Azim memon <87356319+Azi-mx@users.noreply.github.com>
2023-05-04 14:57:51 +0530

    Programs of class

---
+++ Operators/if_else.js
@@ -0,0 +1,37 @@
+let celsius = 32,farenheit;
+
+farenheit = (celsius*9/5)+32;
+document.write(farenheit);
+
+
+
+
+
+
+
+
+
+
+
+/*let salary = 100,gross,HRA,DA,TA;
+HRA = salary * 0.10;
+DA  = salary * 0.05;
+TA  = salary * 0.08;
+
+gross = HRA+DA+TA+salary;
+document.write('The gross salary is '+gross)*/
+
+/*let m1=75,
+    m2=85,
+    m3=97;
+let total  = m1+m2+m3;
+let per  = total/3;
+if(per > 80){
+    document.write('Grade A');
+}
+else if(per > 70 && per < 80){
+    document.write('Grade B')
+}
+else{
+    document.write('Fail')
+}*/
