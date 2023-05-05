e2b158dd2a4469c5fb34d79bb514f3249ae01c5e
Azi-mx <bagsariyaa@gmail.com>
2023-05-06 01:17:35 +0530

    changes

--- exercise/q1.js
+++ exercise/q1.js
@@ -1,9 +1,18 @@
-let a = 10,b = 20;
-let temp = 0;
-document.write("The value of a =" + a + " and b = " + b + " before <br>");
-temp = a;
-a = b;
-b = temp;
-document.write("The value of a =" + a + " and b = " + b + " after");
+// let a = 10,b = 20;
+// let temp = 0;
+// document.write("The value of a =" + a + " and b = " + b + " before <br>");
+// temp = a;
+// a = b;
+// b = temp;
+// document.write("The value of a =" + a + " and b = " + b + " after");
 
+const fruits = ["banana", "apple", "kela"];
+let flen = fruits.length;
+let i;
 
+let text = "<ul>";
+for(i=0;i<flen;i++){
+    text += "<li>" + fruits[i] + "</li>"
+}
+text += "</ul"
+document.getElementById('demo').innerHTML= text;
