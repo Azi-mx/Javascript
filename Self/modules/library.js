55bec4f88fdff316ebc094821af85b086724696f
Azi-mx <bagsariyaa@gmail.com>
2023-05-19 19:41:35 +0530

    changes

---
+++ Self/modules/library.js
@@ -0,0 +1,12 @@
+ let message  = "Hello";
+ function user(name){
+    console.log(`Hello ${name}`);
+}
+ class test{
+    constructor(){
+        console.log("Azim Memon")
+    }
+}
+
+export{message,user,test};
+
---
+++ Self/modules/main.js
@@ -0,0 +1,9 @@
+import { message,user as us,test} from "./library.js";
+//import * as azim from "./library.js";
+
+
+//console.log(azim.message); 
+console.log(message);
+
+us("Azim");
+let a = new test();
