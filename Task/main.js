5105114a2a2f22085d1913167ebcb0be05aab9ba
Azim memon <87356319+Azi-mx@users.noreply.github.com>
2023-06-12 12:44:56 +0530

    add

---
+++ Task/main.js
@@ -0,0 +1,9 @@
+function validateForm(){
+let name = document.getElementById('username').value;
+let email = document.getElementById('email').value;
+let pass = document.getElementById('password').value;
+
+if(name == ""){
+    console.log("Not valid");
+}
+}
