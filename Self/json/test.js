380795b75b6c36c4beccf7c0dd7ccf3157f7e03d
Azi-mx <bagsariyaa@gmail.com>
2023-06-15 12:43:17 +0530

    added

--- Self/json/test.js
+++ Self/json/test.js
@@ -8,7 +8,10 @@ fetch('https://fakestoreapi.com/products')
                     <td>${json[i].id}</td>
                     <td>${json[i].title}</td>
                     <td>${json[i].price}</td>
-                    <td>${json[i].description}</td>
+                    <td id="center">${json[i].description}
+                    <div>
+                        <button id="btn1">Buy</button></td>
+                    </div>
                     <td>${json[i].category}</td>
                     <td><img src=${json[i].image}></td>
                     <td>${json[i].rating.rate} and count  ${json[i].rating.count}</td>
