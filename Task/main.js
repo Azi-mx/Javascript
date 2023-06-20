4ed20eaac5cec64ed2720fcc8467db4da847c074
Azi-mx <bagsariyaa@gmail.com>
2023-06-20 18:01:33 +0530

    changes

--- Task/main.js
+++
@@ -1,9 +0,0 @@
-function validateForm(){
-let name = document.getElementById('username').value;
-let email = document.getElementById('email').value;
-let pass = document.getElementById('password').value;
-
-if(name == ""){
-    console.log("Not valid");
-}
-}
